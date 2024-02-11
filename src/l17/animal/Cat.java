937adaa5bdb0da145214feb2ba50000a5b7563b8
Daria <daria@mail.mail>
2024-02-11 16:21:22 +0100

    init

---
+++ src/l17/animal/Cat.java
@@ -0,0 +1,13 @@
+package l17.animal;
+
+public class Cat {
+    String name;
+    int age;
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {
+        return "Cat, " + this.name + ", " + age;
+    }
+}
