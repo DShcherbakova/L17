cba31bd3040656f7356bc7a71ef0e81a66dd7968
Daria <daria@mail.mail>
2024-02-13 19:35:13 +0100

    init

--- src/l17/CarTest.java
+++ src/l17/CarTest.java
@@ -1,2 +1,44 @@
-package l17;public class CarTest {
+package l17;
+
+import l17.transport.Car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+public class CarTest {
+    private MyArrayList carShowroom;
+    private Car bmw;
+    private Car porsche;
+    private Car lamborghini;
+    private Car ford;
+    private Car ferrari;
+
+    @BeforeEach
+    public void setUp() {
+        this.bmw = new Car("Bmw", 2);
+        this.porsche = new Car("Porsche", 2);
+        this.lamborghini = new Car("Lamborghini", 2);
+        this.ford = new Car("Ford", 2);
+        this.ferrari = new Car("Ferrari", 2);
+
+        this.carShowroom = new MyArrayList();
+
+        carShowroom.add(bmw);
+        carShowroom.add(porsche);
+        carShowroom.add(lamborghini);
+        carShowroom.add(ford);
+        carShowroom.add(ferrari);
+    }
+    @Test
+    public void testGetter() {
+        MyArrayList carShowroomEmpty = new MyArrayList();
+        Assertions.assertEquals(this.bmw, this.carShowroom.getByIndex(0));
+        Assertions.assertEquals(ferrari, carShowroom.getByIndex(4));
+    }
+    @Test
+    public void testCount() {
+        MyArrayList carShowroomEmpty = new MyArrayList();
+        Assertions.assertEquals(carShowroomEmpty.count, 0);
+        Assertions.assertEquals(5, this.carShowroom.count);
+    }
 }
